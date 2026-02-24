98546d4607e2a36238115ce3e6023ac86eda2360
rene_delyan <renedropgamer@gmail.com>
2026-02-24 20:07:55 +0100

    Zwischenstand für OperativeView und OperativeViewModel - Unterbrochen um Setup Logik hinzuzufügen und zu testen.

--- ScooterKonsortiumWPF/ViewModel/OperativeViewModel.cs
+++ ScooterKonsortiumWPF/ViewModel/OperativeViewModel.cs
@@ -1,19 +1,59 @@
-﻿using System;
+﻿using ScooterKonsortium;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Windows.Input;
 
 namespace ScooterKonsortiumWPF.ViewModel {
     public class OperativeViewModel : ViewModelBase {
-        private readonly MainViewModel mainViewModel;
+        //Private properties
+        private readonly MainViewModel       mMainViewModel;
+        private          ScooterViewModel    mSelectedScooter;
 
-        public ICommand BackToMainCommand { get; }
+        //Public properties
+        public double                       mScale => 20;
 
+        //Memberfunctions
+        public ObservableCollection<ScooterViewModel> Scooters {
+            get;
+        }
+        public ScooterViewModel SelectedScooter {
+            get => mSelectedScooter;
+            set {
+                mSelectedScooter = value;
+                OnPropertyChanged ();
+            }
+        }
+
+        //Commands
+        public ICommand MoveCommand {
+            get;
+        }
+
+        //Constructor
         public OperativeViewModel (MainViewModel main)
         {
-            this.mainViewModel = main;
+            this.mMainViewModel = main;
 
-            BackToMainCommand = new RelayCommand (mainViewModel.ShowMainMenu);
+            using var context = new ScooterDbContext ();
+
+            var scootersFromDb = context.Scooters.ToList ();
+
+            Scooters = new ObservableCollection<ScooterViewModel> (
+                scootersFromDb.Select (s => new ScooterViewModel (s))
+            );
+            MoveCommand = new RelayCommand (MoveScooter);
+        }
+
+        //Functions
+        private void MoveScooter ()
+        {
+            if (SelectedScooter == null)
+                return;
+
+            SelectedScooter.PosX += 1;
+            SelectedScooter.PosY += 1;
         }
     }
 }
---
+++ ScooterKonsortiumWPF/ViewModel/ScooterViewModel.cs
@@ -0,0 +1,43 @@
+﻿using ScooterKonsortium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScooterKonsortiumWPF.ViewModel
+{
+    public class ScooterViewModel : ViewModelBase {
+        private readonly Scooter _model;
+
+        public ScooterViewModel (Scooter model)
+        {
+            _model = model;
+        }
+
+        public int Id => _model.Id;
+
+        public int PosX {
+            get => _model.PosX;
+            set {
+                if (_model.PosX != value) {
+                    _model.PosX = value;
+                    OnPropertyChanged ();
+                    OnPropertyChanged (nameof (DrawX));
+                }
+            }
+        }
+
+        public int PosY {
+            get => _model.PosY;
+            set {
+                if (_model.PosY != value) {
+                    _model.PosY = value;
+                    OnPropertyChanged ();
+                    OnPropertyChanged (nameof (DrawY));
+                }
+            }
+        }
+
+        public double DrawX => PosX * 20;
+        public double DrawY => PosY * 20;
+    }
+}
