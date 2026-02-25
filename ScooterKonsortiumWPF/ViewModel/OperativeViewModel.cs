35d4a43f779837e7f11c001283218a3e7f2f8d2e
Bexastockman <aleksey.morosjuk@gmail.com>
2026-02-25 23:17:38 +0100

    MoveScooter: - Accepts only valid x,y values - wont move the scooter if currentbat < 30 oder cost > currentbat - User feedback messageboxes added - CalcDistance function added
    BringScooterToStation
    - calculates closest station to scooter
    - user feedback messagebox added
    - sets currentbat to 100
    - CalcClosestStation function added

--- ScooterKonsortiumWPF/ViewModel/OperativeViewModel.cs
+++ ScooterKonsortiumWPF/ViewModel/OperativeViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Drawing;
 using System.Text;
+using System.Windows;
 using System.Windows.Input;
 
 namespace ScooterKonsortiumWPF.ViewModel {
@@ -80,11 +81,42 @@ namespace ScooterKonsortiumWPF.ViewModel {
         //Functions
         private void MoveScooter ()
         {
+            //Max X = 19 Max Y = 32
             if (SelectedScooter == null)
                 return;
 
-            SelectedScooter.PosX += 1;
-            SelectedScooter.PosY += 1;
+            int distance = 0;
+            int chargeCost = 0;
+            
+
+            distance = CalcDistance(SelectedScooter.PosX, SelectedScooter.PosY,ChangePosX,ChangePosY);
+            chargeCost = distance * 2;
+            if (ChangePosX > 19 || ChangePosX < 0 || ChangePosY > 32 || ChangePosY < 0)
+            {
+                MessageBox.Show("Gültige Koordinaten eingeben! X (0-19) | Y (0 - 32)");
+            }
+            else if(SelectedScooter.PosX == ChangePosX && SelectedScooter.PosY == ChangePosY)
+            {
+                MessageBox.Show("Die Koordinaten müssen sich vom aktuellen Standort unterscheiden!");
+            }
+
+            else if(SelectedScooter.CurrentBattery > 30 && (SelectedScooter.CurrentBattery - chargeCost) > 0)
+            {
+               
+                SelectedScooter.PosX = ChangePosX;
+                SelectedScooter.PosY = ChangePosY;
+                SelectedScooter.CurrentBattery -= chargeCost;
+                MessageBox.Show("Der Scooter " + SelectedScooter.Id + " ist erfolgreich am Ziel (" + SelectedScooter.PosX + "/" + SelectedScooter.PosY + ") angekommen!\nNeue Reichweite: " + SelectedScooter.CurrentBattery + "%");
+            }
+            else if(SelectedScooter.CurrentBattery <= 30)
+            {
+                MessageBox.Show("Scooter " + SelectedScooter.Id + " hat nicht genug Reichweite für eine Fahrt und muss zur nächsten Ladestation gebracht werden!\nAktuelle Reichweite: " + SelectedScooter.CurrentBattery);
+            }
+            else if(SelectedScooter.CurrentBattery < chargeCost) 
+            {
+                MessageBox.Show("Scooter " + SelectedScooter.Id + " hat nicht genug Reichweite für diese Fahrt! Bitte eine geringere Distanz auswählen oder zur nächsten Ladestation bringen.\n+" +
+                                "Aktuelle Reichweite: " + SelectedScooter.CurrentBattery + "\nBenötigte Reichweite: " + chargeCost);
+            }
             //SelectedScooter.CurrentBattery
         }
 
@@ -92,11 +124,42 @@ namespace ScooterKonsortiumWPF.ViewModel {
         {
             if (SelectedScooter == null)
                 return;
-            var station = mDbContext.chargingStations.FirstOrDefault ();
+            //var station = mDbContext.chargingStations.FirstOrDefault ();
+            var station = CalcClosestStation();
+       
             if (station != null) {
                 SelectedScooter.PosX = station.PosX;
                 SelectedScooter.PosY = station.PosY;
+                SelectedScooter.CurrentBattery = 100;
+                MessageBox.Show ("Der Scooter " + SelectedScooter.Id + " wurde zur nächsten Ladestation gebracht und vollständig aufgeladen!");
             }
         }
+        private int CalcDistance(int oldX,int oldY,int newX,int newY)
+        {
+            int distance = 0;
+            distance = Math.Abs (oldX - newX) + Math.Abs (oldY - newY);
+            return distance;
+        }
+        private ChargingStationViewModel CalcClosestStation()
+        {
+            int min = int.MaxValue;
+            int distance = 0;
+
+            ChargingStationViewModel nearestStation = null;
+            
+            foreach (var station in ChargingStations) 
+            {
+                distance = CalcDistance(SelectedScooter.PosX,SelectedScooter.PosY,station.PosX,station.PosY);
+                if(distance < min)
+                {
+                    min = distance;
+                    nearestStation = station;
+                }
+            }
+
+            return nearestStation;
+        }
+
+       
     }
 }
